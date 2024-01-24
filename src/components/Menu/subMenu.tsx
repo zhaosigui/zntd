e7bb06149e28cf9d616c36b98f17cbb6a88e6621
zhaosigui <zhaosigui@126.com>
2024-01-24 21:18:46 +0800

    subMenu 添加icon 旋转效果

--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import classNames from "classnames";
 import { MenuContext } from "./menu";
 import { MenuItemProps } from "./menuItem";
+import Icon from "../Icon";
 export interface SubMenuProps {
   index?: string;
   title: string;
@@ -21,6 +22,8 @@ const SubMenu: React.FC<SubMenuProps> = ({
   const [menuOpen, setOpen] = useState(isOpened);
   const classes = classNames("menu-item submenu-item", className, {
     "is-active": context.index === index,
+    'is-opened': menuOpen,
+    'is-vertical': context.mode === 'vertical'
   });
   const handleClick = (e: React.MouseEvent) => {
     e.preventDefault();
@@ -75,6 +78,7 @@ const SubMenu: React.FC<SubMenuProps> = ({
     <li key={index} className={classes} {...hoverEvents}>
       <div className="submenu-title" {...clickEvents}>
         {title}
+        <Icon icon="angle-down" className="arrow-icon"/>
       </div>
       {renderChildren()}
     </li>
