766bbe727279ebf8430ad686fb68dd2f62d3e454
zhaosigui <zhaosigui@126.com>
2024-01-24 16:51:27 +0800

    menu 组件添加

---
+++ src/components/Menu/menu.test.tsx
@@ -0,0 +1,89 @@
+/* eslint-disable testing-library/render-result-naming-convention */
+import React from "react";
+import {
+  render,
+  screen,
+  fireEvent,
+  RenderResult,
+  cleanup,
+} from "@testing-library/react";
+import Menu, { MenuProps } from "./menu";
+import MenuItem from "./menuItem";
+import { WritableStream } from "node:stream/web";
+
+const testProps: MenuProps = {
+  defaultIndex: '0',
+  onSelect: jest.fn(),
+  className: "test",
+};
+const testVerProps: MenuProps = {
+  defaultIndex: '0',
+  mode: "vertical",
+};
+const generateMenu = (props: MenuProps) => {
+  return (
+    <Menu {...props}>
+      <MenuItem>active</MenuItem>
+      <MenuItem disabled>
+        disabled
+      </MenuItem>
+      <MenuItem>default</MenuItem>
+    </Menu>
+  );
+};
+let menuElement: HTMLElement,
+  activeElement: HTMLElement,
+  disabledElement: HTMLElement;
+describe("test Menu and MenuItem component in default(horizontal) mode", () => {
+    // 每次测试前开始执行, 用于合并相同的测试逻辑
+  beforeEach(() => {
+    // eslint-disable-next-line testing-library/no-render-in-setup
+    const wrapper = render(generateMenu(testProps));
+    // wrapper.container.append(createStyleFile())
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    menuElement = wrapper.getByTestId("test-menu");
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    activeElement = wrapper.getByText("active");
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    disabledElement = wrapper.getByText("disabled");
+  });
+  // let { container, rerender, queryByText, getByTestId, unmount } = render(
+  //   generateMenu(testProps)
+  // );
+  // eslint-disable-next-line testing-library/render-result-naming-convention
+  // eslint-disable-next-line testing-library/no-debugging-utils
+  screen.debug();
+  // beforeEach(() => {
+  // wrapper.container.append(createStyleFile())
+  menuElement = screen.getByTestId("test-menu");
+  activeElement = screen.getByText("active");
+  disabledElement = screen.getByText("disabled");
+  // });
+  it("should render corrent Menu and  MenuItem base on default props", () => {
+    expect(menuElement).toBeInTheDocument();
+    expect(menuElement).toHaveClass("zntd-menu test");
+
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    // expect(container.getElementsByTagName("li").length).toEqual(3);
+
+    // eslint-disable-next-line testing-library/no-node-access,
+    expect(menuElement.getElementsByTagName("li").length).toEqual(3);
+    expect(activeElement).toHaveClass("menu-item is-active");
+    expect(disabledElement).toHaveClass("menu-item is-disabled");
+  });
+  it("click items should change active and call the right callback", () => {
+    const thirdItem = screen.getByText("default");
+    const activeEl = screen.getByText("active");
+    const disabledEl = screen.getByText("active");
+    fireEvent.click(thirdItem);
+    expect(thirdItem).toHaveClass("is-active");
+    expect(activeEl).not.toHaveClass("is-active");
+    expect(testProps.onSelect).toHaveBeenCalledWith(2);
+    fireEvent.click(disabledEl);
+    expect(disabledEl).not.toHaveClass("is-active");
+    expect(testProps.onSelect).not.toHaveBeenCalledWith(1);
+  });
+  it("should render vertical mode when mode is set to vertical", () => {
+    render(generateMenu(testVerProps));
+  });
+});
---
+++ src/components/Menu/menu.tsx
@@ -0,0 +1,85 @@
+/*
+ * @Author: zhaosigui
+ * @Date: 2024-01-23 15:14:49
+ * @LastEditors: zhaosigui
+ * @LastEditTime: 2024-01-24 16:44:48
+ * @FilePath: \antd\zntd\src\components\Menu\menu.tsx
+ * @Description:
+ */
+import React, { useState } from "react";
+import classNames from "classnames";
+import { MenuItemProps } from "./menuItem";
+type Mode = "horizontal" | "vertical";
+type SelectCallback = (selectIndex: string) => void;
+export interface MenuProps {
+  defaultIndex?: string;
+  className?: string;
+  mode?: Mode;
+  style?: React.CSSProperties;
+  onSelect?: SelectCallback;
+  children?: React.ReactNode;
+  // 默认展开项
+  defaultOpenSubMenus?: string[];
+}
+interface IMenuContext {
+  index: string;
+  onSelect?: SelectCallback;
+  mode?: Mode;
+  defaultOpenSubMenus?: string[]
+}
+export const MenuContext = React.createContext<IMenuContext>({
+  index: "0",
+});
+const Menu: React.FC<MenuProps> = (props) => {
+  const { children, className, defaultIndex, mode, style, onSelect, defaultOpenSubMenus } = props;
+  const [currentActive, setActive] = useState(defaultIndex);
+  const classes = classNames("zntd-menu", className, {
+    "menu-vertical": mode === "vertical",
+    "menu-horizontal": mode !== "vertical",
+  });
+  const handleClick = (index: string) => {
+    setActive(index);
+    if (onSelect) {
+      onSelect(index);
+    }
+  };
+  // 这是为了遵循react 自顶向下的数据流规则，所以采用
+  const passedContext: IMenuContext = {
+    index: currentActive ? currentActive : "0",
+    onSelect: handleClick,
+    mode,
+    defaultOpenSubMenus
+  };
+  const renderChildren = () => {
+    return React.Children.map(children, (child, index) => {
+      const childElement =
+        child as React.FunctionComponentElement<MenuItemProps>;
+      const { displayName } = childElement.type;
+      if (displayName === "MenuItem" || displayName === "SubMenu") {
+        // 将index 混入到元素属性中, 可以避免必传index
+        const cloneElement = React.cloneElement(childElement, {
+          index: index.toString(),
+        });
+        return cloneElement;
+      } else {
+        console.error(
+          "Warning: Menu has a child which is not a MenuItem component"
+        );
+      }
+    });
+  };
+  return (
+    <ul className={classes} style={style} data-testid="test-menu">
+      <MenuContext.Provider value={passedContext}>
+        {renderChildren()}
+      </MenuContext.Provider>
+    </ul>
+  );
+};
+Menu.defaultProps = {
+  defaultIndex: "0",
+  mode: "horizontal",
+  defaultOpenSubMenus: []
+  
+};
+export default Menu;
