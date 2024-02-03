122737365847b01f7969851899f9b7e04d0ff98c
zhaosigui <zhaosigui@126.com>
2024-02-03 18:13:27 +0800

    autocomplete 基础模板

--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, ReactElement } from "react";
+import React, { useState, useEffect, ReactElement, ChangeEvent } from "react";
 import classNames from "classnames";
 import Input, { InputProps } from "../Input/input";
 
@@ -19,24 +19,63 @@ export interface AutoCompleteProps
   /** 点击选中建议项时触发的回调*/
   onSelect?: (item: DataSourceType) => void;
   /** 文本框发生改变的时候触发的事件*/
-  onChange?: (value: string) => void;
+  // onChange?: (value: string) => void;
   /**支持自定义渲染下拉项，返回 ReactElement */
   renderOption?: (item: DataSourceType) => ReactElement;
   data: string[];
 }
 
-
 /**
  * 输入框自动完成功能。当输入值需要自动完成时使用，支持同步和异步两种方式
  * 支持 Input 组件的所有属性 支持键盘事件选择
  * ### 引用方法
- * 
+ *
  * ~~~js
  * import { AutoComplete } from 'zntd'
  * ~~~
  */
 // 这里export是为了迎合storybook自动生成文档
 export const AutoComplete: React.FC<AutoCompleteProps> = (props) => {
-  return null;
+  const { onSelect, fetchSuggestions, value, ...restsProps } = props;
+  const [inputValue, setInputValue] = useState(value);
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setInputValue(value);
+    if (value) {
+      const results = fetchSuggestions(value);
+      setSuggestions(results);
+    } else {
+      setSuggestions([]);
+    }
+  };
+  const handleSelect = (item: string) => {
+    setInputValue(item);
+    setSuggestions([]);
+    if (onSelect) {
+      onSelect(item);
+    }
+  };
+  const generateDropDown = () => {
+    return (
+      <ul>
+        {suggestions.map((item, index) => {
+          return (
+            <li key={index} onClick={() => handleSelect(item)}>
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
+  return (
+    <>
+      <div className="zntd-auto-complete">
+        <Input value={inputValue} onChange={handleChange} {...restsProps} />
+        {suggestions.length && generateDropDown()}
+      </div>
+    </>
+  );
 };
 export default AutoComplete;
